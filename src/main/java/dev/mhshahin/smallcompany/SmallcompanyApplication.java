f64300d42b385791e9901fa88507117828225edc
mahmoudshahin-it <mahmoudshahin.it@gmail.com>
2023-01-08 11:32:05 +0200

    Initial commit - No changes yet. Only key dependencies are there.

---
+++ src/main/java/dev/mhshahin/smallcompany/SmallcompanyApplication.java
@@ -0,0 +1,13 @@
+package dev.mhshahin.smallcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallcompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallcompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mhshahin/smallcompany/SmallcompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mhshahin.smallcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallcompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
